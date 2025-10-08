2b88b551111920f1f7f6c3a124b83b13c71c1349
NguyenCuong3004 <vancuong2k3@gmail.com>
2025-10-08 22:26:33 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Laravel + Tailwind + Vite</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  </head>
+  <body class="bg-gray-100 flex items-center justify-center min-h-screen">
+    <h1 class="text-4xl font-bold text-blue-600">Hello LaravelCow with Tailwind 🎉</h1>
+  </body>
+</html>
